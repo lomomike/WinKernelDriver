37b7cb1a0765a0c3afc75a01eb82397871e923f3
Mikhail Polgun <mikhailpolgun@gmail.com>
2016-09-12 15:55:45 +0300

    Load/unload driver from starter

--- DriverStarter/DriverStarter.cpp
+++ DriverStarter/DriverStarter.cpp
@@ -12,6 +12,9 @@
 #define DEVICE_NAME L"\\\\.\\Testdrv"
 #define IOCTL_CODE CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
+#define DRIVER_NAME TEXT("testdrv")
+#define DRIVER_FILENAME TEXT("C:\\Users\\Administrator\\Desktop\\Driver2\\TestDriver.sys")
+
 
 const char message[] = "Greetings from user mode";
 
@@ -26,15 +29,15 @@ void debug(char *text) {
 	printf("%s: %S\n", text, msgbuf);
 }
 
-int wmain(int argc, wchar_t* argv[])
+void CommunicateWithDriver()
 {
 	HANDLE hDevice;
 	ULONG outBuf;
 	DWORD returned;
 
 	hDevice = CreateFile(DEVICE_NAME, GENERIC_READ | GENERIC_WRITE,
-		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL, NULL);
+	                                FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING,
+	                                FILE_ATTRIBUTE_NORMAL, NULL);
 
 	if (hDevice == INVALID_HANDLE_VALUE) {
 		debug("error opening device");
@@ -61,6 +64,62 @@ int wmain(int argc, wchar_t* argv[])
 
 		CloseHandle(hDevice);
 	}
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+	auto hSCMAnager = OpenSCManager(nullptr, nullptr, SC_MANAGER_CREATE_SERVICE);
+	printf("Load driver...\n");
+	if(hSCMAnager)
+	{
+		printf("Create service...\n");
+		wprintf(TEXT("Driver path: %s\n"), DRIVER_FILENAME);
+
+		auto hService = CreateService(
+			hSCMAnager,
+			DRIVER_NAME,
+			DRIVER_NAME,
+			SERVICE_START | DELETE | SERVICE_STOP,
+			SERVICE_KERNEL_DRIVER,
+			SERVICE_DEMAND_START,
+			SERVICE_ERROR_IGNORE,
+			DRIVER_FILENAME,
+			nullptr, nullptr, nullptr, nullptr, nullptr);
+		debug("Create service");
+
+		if (!hService)
+		{
+			hService = OpenService(hSCMAnager, DRIVER_NAME, SERVICE_START | DELETE);
+		}
+
+		if (hService)
+		{
+			printf("Start service...\n");
+
+			StartService(hService, 0, nullptr);
+			debug("Starting service");
+
+			CommunicateWithDriver();
+
+			SERVICE_STATUS ss;
+			ControlService(hService, SERVICE_CONTROL_STOP, &ss);
+
+			DeleteService(hService);
+			CloseServiceHandle(hService);			
+		}
+		else
+		{
+			debug("error occured");
+		}
+
+		CloseServiceHandle(hSCMAnager);
+	}
+	else
+	{
+		debug("Error occured");
+	}
+
+	
 
 	return 0;
 }
